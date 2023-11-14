e303095599b7ba9be5403e65374d6bc30e3caebb
EricoMartin <ericomartin.0785@gmail.com>
2023-11-14 12:20:20 -0800

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -35,31 +35,32 @@ class FileStorage:
             obj: The current object to get its id
         """
 
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        key = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(key, obj.id)] = obj
 
     def save(self):
         """ Serializes __objects to a JSON file """
 
-        my_list = {}
-        for obj in FileStorage.__objects.keys():
-            new_obj = FileStorage.__objects[obj].to_dict()
-            my_list.update(new_obj)
+        my_dict = FileStorage.__objects
+        obj_dict = {}
+        for obj in my_dict.keys():
+            obj_dict[obj] = my_dict[obj].to_dict()
         
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(my_list, file)
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as f:
+            json.dump(obj_dict, f)
 
     def reload(self):
         """ deserializes a JSON file to __objects 
             (only if the JSON file (__file_path) exists
         """ 
         try:
-            with open(FileStorage.__file_path, "r") as file:
-                new_data = json.load(file)
+            with open(FileStorage.__file_path) as f:
+                new_data = json.load(f)
 
                 for val in new_data.values():
-                    del val['__class__']
-                    self.new(eval[val['__class__']](**val))            
+                    cls_name = val["__class__"]
+                    del val["__class__"]
+                    self.new(eval(cls_name)(**val))            
 
         except Exception:
                 pass
