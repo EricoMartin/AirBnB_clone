d777f8f0253881b0bad04921c1c7c592c0f1f59c
EricoMartin <ericomartin.0785@gmail.com>
2023-11-15 08:13:21 -0800

    models

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,11 +8,12 @@ import json
 import os
 from models.base_model import BaseModel
 from models.user import User
-
-new_dict = {
-    "BaseModel": BaseModel,
-    "User": User
-    }
+from models.user import User
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
+from models.review import Review
 
 
 class FileStorage:
--- models/state.py
+++ models/state.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+""" State model module """
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Models a state.
+
+    Attributes:
+        name (str): The name of the state.
+    """
+
+    name = ""
