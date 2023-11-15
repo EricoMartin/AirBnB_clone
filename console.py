2aab1aeee9bc38aff243d5acb66ffa213df1e47e
EricoMartin <ericomartin.0785@gmail.com>
2023-11-15 04:18:39 -0800

    console.py

--- console.py
+++ console.py
@@ -7,11 +7,11 @@ class HBNBCommand(cmd.Cmd):
     """ A console class that contains the entry point of the
         command interpreter
     """
-    prompt = (hbnb)
+    prompt = "(hbnb)"
 
 
     def do_quit(self, arg):
-    """Quit command to exit the program"""
+        """Quit command to exit the program"""
         do_EOF()
 
     def do_EOF(self, line):
@@ -23,5 +23,5 @@ class HBNBCommand(cmd.Cmd):
         return self.prompt
 
     if __name__ == '__main__':
-    HBNBCommand().cmdloop()
+        HBNBCommand().cmdloop()
 
