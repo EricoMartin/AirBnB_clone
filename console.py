a670d72ea9f1686e1ab993a0d5f365a8d79cc87d
EricoMartin <ericomartin.0785@gmail.com>
2023-11-15 04:45:52 -0800

    console.py

--- console.py
+++ console.py
@@ -7,10 +7,10 @@ class HBNBCommand(cmd.Cmd):
     """ A console class that contains the entry point of the
         command interpreter
     """
-    prompt = "(hbnb)"
+    prompt = "(hbnb) "
 
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program"""
         return True
 
@@ -18,10 +18,9 @@ class HBNBCommand(cmd.Cmd):
         """ Returns the end of file """
         return True
 
-    def emptyline(self, line):
+    def emptyline(self):
         """ Override the emptyline method """
-        return self.prompt
+        pass
 
-    if __name__ == '__main__':
-        HBNBCommand().cmdloop()
-
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
